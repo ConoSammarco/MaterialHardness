5df0b89ea5b99122f26c0bd57ebd605bd03ed30b
ConoSammarco <67301387+ConoSammarco@users.noreply.github.com>
2020-08-09 10:57:02 -0400

    Calculations Programs
    These python programs make the calculation of Hardness, Ionocity, and the Ionic contribution to Eg a very quick and easy task. Ideally, there will be a new combined version being released in the future.

---
+++ HardnessCalc.py
@@ -0,0 +1,38 @@
+import math
+
+
+
+#This Program needs all the  bond infromation inputted and will output the theoretical hardness of the material
+#The mathematics behind this can be view in Dr.Chee-Keong Tans research.
+#This program utilizes the vickters scale as a hardness comparison.
+#This Program was created by: Cono Sammarco (Clarkson University)
+#This program will need to be ran once for every bond length used
+
+
+#Dependent Variables
+ValPerVolume = 0.00 #Valence Electrons per Cubic Angstrom of Bond
+NumberOfBonds = 2 #Number of Bonds (minimum of one)
+
+
+
+
+############# CALCULATIONS ###############
+Hardness = 1.00 #DONOT CHANGE THIS VARIABLE
+H = 1.00 #DONOT CHANGE THIS VARIABLE
+for i in range(0,NumberOfBonds):
+    print('Bond number: '+ str(i+1))
+    Bondlength = input('Length of the bond [Angstroms]: ')
+    N = input('Valence electrons per cubic angstrom of the bond: ')
+    Fi = input('Ionocity of the Bond: ')
+    Fi = float(Fi)
+    N = float(N)
+    Bondlength = float(Bondlength)
+    I = 1.191 * Fi
+
+    H = (350 * pow(N, (2/3))/ (pow(2.71828, I) * pow(Bondlength,2.5)))
+    Hardness = Hardness * H
+
+
+Hardness = pow(Hardness, 0.5)
+
+print('Total Hardness: '+ str(Hardness) + '[Gpa]')
